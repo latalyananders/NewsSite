e0c1703a837bb4a5a65775e53a111eecf81954c1
Garanina Natalya <garaninanatalya3@gmail.com>
2017-10-09 23:08:41 +0400

    Даже не дышите в сторону этого сайта ୧((#Φ益Φ#))୨
    В общем, я верстала, верстала и что-то выверстала (ノ*°▽°*)
    Получилось не очень красиво, особенно поле поиска, но потом я, может
    быть, найду способ сделать это чуть лучше (´｡• ᵕ •｡`)
    Главное, что ОНО не съезжает ни при просмотре с телефона, ни при
    просмотре с ноутбука (◕‿◕)
    Однако нужно учесть, что у моего ноутбука маленькое разрешение экрана,
    поэтому я даже боюсь представить, как оно будет выглядеть на нормальном
    разрешении ヾ(。＞＜)シ
    Но мы не унываем и берём от жизни что дают ( ´ ∀ `)ノ～ ♡

---
+++ site/index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" >
+        <title>Самые новые новости</title>
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+        <link rel="stylesheet" href="css/bootstrap-theme.css">
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/bootstrap.css">
+    </head>
+    <body>
+        <div class="container">
+            <div class="main-rec">
+                <h1 class="hero-unit">
+                    <a class="brand" href="#" style="color: white">
+                        <strong>
+                            <em>
+                                САМЫЕ<br>НОВЫЕ<br>НОВОСТИ
+                            </em>
+                        </strong>
+                    </a>
+
+                </h1>
+            </div>
+            <div class="admin-rec">
+                <div class="row">
+                    <button class="btn col-sm-2 "
+                            style="
+                        margin-left: 50px;
+                        width: 150px;
+                        margin-top: 3px;
+                        background-color: white;
+                        border-color: black;"
+                    >Добавить новость</button>
+                    <input type="text" placeholder="Поиск по сайту..."
+                           style="margin-top: 3px; /*margin-left: 540px;*/ height: 33px;
+                           padding-left: 15px"
+                           class="col-sm-2 col-sm-offset-5">
+                    <button class="btn col-sm-1 " style="
+                        margin-left: 5px;
+                        margin-right: 40px;
+                        margin-top: 3px;
+                        background-color: white;
+                        border-color: black;"
+                    >Поиск</button>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
